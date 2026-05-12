e1f7296ca55b0488ba89bb868368226268c92780
Łukasz Nowak <lukasz.nowak.programista@gmail.com>
2026-05-12 16:39:21 +0200

    deleting methods for tests

--- main.cpp
+++ main.cpp
@@ -4,26 +4,6 @@
 
 using namespace std;
 
-int main_test()
-{
-    KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy2.txt", "Adresaci2.txt");
-
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
-    ksiazkaAdresowa.logowanieUzytkownika();
-    ksiazkaAdresowa.logowanieUzytkownika();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-    ksiazkaAdresowa.wylogowanieUzytkownika();
-    ksiazkaAdresowa.logowanieUzytkownika();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-    ksiazkaAdresowa.wylogowanieUzytkownika();
-
-    return 0;
-}
-
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy2.txt", "Adresaci2.txt");
